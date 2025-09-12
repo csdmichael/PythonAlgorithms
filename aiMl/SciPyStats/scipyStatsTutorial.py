6874bbdff7de87d615060c712b1a0c3db0178de6
csdmichael <csdmichael@gmail.com>
2025-09-12 12:22:53 -0700

    cdf

--- aiMl/SciPyStats/scipyStatsTutorial.py
+++ aiMl/SciPyStats/scipyStatsTutorial.py
@@ -8,6 +8,13 @@ U = uniform(loc=10, scale=5)  # Uniform distribution between 0 and 1
 x = U.pdf([8, 12, 20]) #probability density function
 print(x)
 
+p_less_than_12 = U.cdf(12)
+cdf_11 = U.cdf(11)
+cdf_13 = U.cdf(13)
+p_between_11_and_13 = cdf_13 - cdf_11
+p_greater_than_12 = 1 - p_less_than_12
+print(p_less_than_12, p_between_11_and_13, p_greater_than_12)
+
 U = uniform(loc=2, scale=6)  # Uniform distribution between 2 and 8
 m = U.mean() #mean
 v = U.var()  #variance
@@ -15,7 +22,8 @@ s= U.std()  #standard deviation
 
 print(m, v, s)
 
-
+y = U.cdf(x<5)
+print('y', y) #cumulative distribution function
 
 #Sampling
 samples = U.rvs(size=3) #random variates
