537fdc12b2badcc88bc0b72b78e3a8a4891022b2
csdmichael <csdmichael@gmail.com>
2025-09-12 18:46:41 -0700

    baseBall Salaries Uniform distrib

---
+++ aiMl/SciPyStats/baseballSalaries.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+#read in the data
+baseball_salaries = pd.read_csv('aiMl/SciPyStats/data/baseball.csv', index_col=0)
+
+#summary statistics
+summary = baseball_salaries.describe()
+print(summary)
+
+#plot a histogram of the salaries
+sampleSize = 1000
+U = stats.uniform(loc=baseball_salaries['salary'].min(), 
+                  scale=baseball_salaries['salary'].max()-baseball_salaries['salary'].min())
+samples = U.rvs(size=sampleSize) #random variates
+
+mean = samples.mean()
+std = samples.std()
+
+print(f"Uniform Distribution for Sample size = {sampleSize}: mean = {mean}, std = {std}")
+
+plt.hist(samples, bins=10, density=True, alpha=0.5, color='g')
+plt.title(f'Uniform Distribution of Baseball Salaries, Sample Size = {sampleSize}')
+plt.xlabel('Salary')
+plt.ylabel('Density')
+#plt.show()
+
+
+
